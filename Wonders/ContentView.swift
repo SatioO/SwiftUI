b1fc020cc2f76438c7681f222941c15772ab7d07
SatioO <vaibhav.satam29991@gmail.com>
2020-07-09 15:47:20 +0530

    Initial Commit

---
+++ Wonders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wonders
+//
+//  Created by Brain on 7/9/20.
+//  Copyright © 2020 Brain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
