717590bd3693abb7028a56f6bb49f9a7dedc57b0
SatioO <vaibhav.satam29991@gmail.com>
2020-07-10 11:55:24 +0530

    added list view

--- Wonders/ContentView.swift
+++ Wonders/ContentView.swift
@@ -7,10 +7,65 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView : UIViewRepresentable {
+    let request: URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView  {
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true;
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preferences
+        
+        let webview = WKWebView(frame: CGRect.zero, configuration: configuration)
+        webview.allowsBackForwardNavigationGestures = true
+        webview.scrollView.showsVerticalScrollIndicator = false
+        webview.scrollView.showsHorizontalScrollIndicator = false
+        
+        return webview
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+}
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+class PostActions {
+    func getPosts(callback: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            
+            DispatchQueue.main.async {
+                callback(posts)
+            }
+        }.resume()
+    }
+}
 
 struct ContentView: View {
+    @State private var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(posts, id: \.id) { post in
+                Text(post.title)
+            }
+        }
+        .onAppear {
+            PostActions().getPosts { (posts) in
+                self.posts = posts
+            }
+        }
     }
 }
 
